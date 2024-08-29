3c7d8a3996eec26911fe188d3a113007ec102f69
DiptiVats <diptivats6@gmail.com>
2024-08-29 13:13:53 +0530

    commiting

--- src/util/auth.js
+++ src/util/auth.js
@@ -1,20 +1,26 @@
 import { redirect } from "react-router";
-
+import { url } from "./url";
 export function getAuthToken() {
   const token = localStorage.getItem("token");
   return token;
 }
 export async function tokenLoader() {
   const token = localStorage.getItem("token");
-  if (!token) {
+  console.log(token);
+  if (token) {
+    const response = await fetch(`${url}/Authentication/staticAuthentication`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response);
+    if (!response.ok) {
+      localStorage.removeItem("token");
+      return redirect("/");
+    }
+  } else {
     return redirect("/");
   }
   return null;
-}
-
-export async function checkExpirationToken(isFourHundred) {
-  if (true) {
-    localStorage.removeItem("token");
-    return redirect("/");
-  }
 }
